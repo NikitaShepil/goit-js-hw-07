30be415b84aedd31dae3e95988b49b3e494f6462
NikitaShepil <121100650+NikitaShepil@users.noreply.github.com>
2023-04-19 14:20:04 +0300

    made homework

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,39 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const listEl = document.querySelector('.gallery');
+function createHtml (items) {
+    return items.map((item) => {
+       return `<li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+          <img
+            class="gallery__image"
+            src="${item.preview}"
+            data-source="${item.original}"
+            alt="${item.description}"
+          />
+        </a>
+      </li>`
+    }).join('');
+}
+listEl.insertAdjacentHTML('beforeend',createHtml(galleryItems) );
+
+listEl.addEventListener('click' ,onImgClick )
+function onImgClick(evt){
+    evt.preventDefault();
+    if(evt.target.nodeName !== 'IMG'){
+        return;
+    }
+    const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+`)
+
+instance.show()
+
+    listEl.addEventListener('keydown', (evt) =>{
+        if(evt.code === 'Escape'){
+            instance.close();
+            document.removeEventListener("keydown", onEscPress);
+        }
+    })
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,22 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const listEl = document.querySelector('.gallery');
+function createHtml (items) {
+    return items.map((item) => {
+       return `<li class="gallery__item">
+       <a class="gallery__link" href="${item.original}">
+          <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+       </a>
+    </li>`
+    }).join('');
+}
+listEl.insertAdjacentHTML('beforeend',createHtml(galleryItems) );
+
+const gallery = new SimpleLightbox('.gallery__item a',{
+    captions: true,
+    captionDelay: 250,
+    captionsData: 'alt',
+    captionsPosition: 'bottom',
+  }) 
